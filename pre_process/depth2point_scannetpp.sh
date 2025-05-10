b53ecf87de3514fc6d61df8323d5e7bd2c9597d1
Paipai-zxa <xiaoanzhang123@gmail.com>
2025-05-10 10:30:22 +0800

    update_param

--- pre_process/depth2point_scannetpp.sh
+++ pre_process/depth2point_scannetpp.sh
@@ -1,4 +1,5 @@
-scene_list=(8b5caf3398 8d563fc2cc 41b00feddb b20a261fdf 1ada7a0617 5748ce6f01 f6659a3107)
+# scene_list=(8b5caf3398 8d563fc2cc 41b00feddb b20a261fdf 1ada7a0617 5748ce6f01 f6659a3107)
+scene_list=(1ada7a0617 5748ce6f01 f6659a3107)
 
 scene=${scene_list[$1]}
 
